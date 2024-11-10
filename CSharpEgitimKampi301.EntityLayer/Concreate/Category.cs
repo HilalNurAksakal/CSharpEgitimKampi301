bfd45bf53df6e52c2e541fc450006e722200054c
hilalnur1010@gmail.com <hilalnur1010@gmail.com>
2024-11-10 13:00:24 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Category.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Category
+    {
+        //Acces Modifiers -> public, private, internal, protected
+        //publıc -> uygulamadaki her yerden erişim
+        //private -> sadece bulunduğu alandan
+        //internal -> sadece bulunduğu katman
+        // protected -> miras alan yerlerden erişim sağlar.
+
+        public int CategoryId { get; set; } //sondaId olması primary ve
+                                            // otomatik artan olarak ayarlamasını sağlar
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+        // product'a category ekledik çünkü her ürünün
+        // tek kategorisi olacak ama 
+        // Category'e Products ekledik her categoryde
+        // birden çok ürün olacak
+
+    }
+}
+/*
+ Field - Variable - Property
+
+int x; --> Field
+public int y{get;set;} --> Property
+void test()
+{
+  int z;  --> Variable
+}
+ */
---
+++ CSharpEgitimKampi301.EntityLayer/Concreate/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concreate
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerDistrict { get; set; }
+        public string CustomerCity { get; set; }
+        public List<Order> Orders { get; set; }
+        public bool CustomerStatus { get; set; }
+
+    }
+}
+/* class ilgili tabloya ait bilgileri tutar */
+
+/*
+ *            Solid
+ *            
+ * Single Responsibility her şeyin ayrı tutulması müşteri-sipaiş
+ * 
+ * 
+ * 
+ * 
+ */
