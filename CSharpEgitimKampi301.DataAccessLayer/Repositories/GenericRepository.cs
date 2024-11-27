80a43d585a4a2a86ce4a78e96f010a5c5862146d
hilalnur1010@gmail.com <hilalnur1010@gmail.com>
2024-11-27 12:41:05 +0300

    Ders 18

---
+++ CSharpEgitimKampi301.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using CSharpEgitimKampi301.DataAccessLayer.Abstract;
+using CSharpEgitimKampi301.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        KampContext context = new KampContext();
+        private readonly DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object=context.Set<T>();
+            //GenericRepository classı çağırıldığı anda
+            //objecte bir nesne örneği oluşturuyor ve buna context
+            //sınıfından gönderdiğimiz entityi atıyor
+        }
+        public void Delete(T entity)
+        {
+            var deletedEntity = context.Entry(entity);
+            deletedEntity.State = EntityState.Deleted; 
+            //EntityState ekleme güncelleme silme işlemleri için kullanabileceğimiz
+            //entity freamwork kodları
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            var addedEntity=context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            var updatedEntity=context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
