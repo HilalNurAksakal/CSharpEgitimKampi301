f669622ffe365dc7d503371cd002778c361e5001
hilalnur1010@gmail.com <hilalnur1010@gmail.com>
2024-12-10 01:19:46 +0300

    Ders 19

--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace CSharpEgitimKampi301.PresentationLayer
 {
-    public partial class Form1 : Form
+    public partial class FrmCategory : Form
     {
-        public Form1()
+        public FrmCategory()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
