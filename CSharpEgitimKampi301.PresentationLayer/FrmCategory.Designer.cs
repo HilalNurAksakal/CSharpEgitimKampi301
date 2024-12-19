2adcbdb83588c4586fd1c4731c08553c47ccfd9c
hilalnur1010@gmail.com <hilalnur1010@gmail.com>
2024-12-19 18:05:00 +0300

    Ders 20

--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -61,6 +61,7 @@
             this.btnList.TabIndex = 1;
             this.btnList.Text = "Listele";
             this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
             // 
             // txtCategortId
             // 
@@ -73,11 +74,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(302, 40);
+            this.dataGridView1.Location = new System.Drawing.Point(298, 31);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(486, 160);
+            this.dataGridView1.Size = new System.Drawing.Size(548, 299);
             this.dataGridView1.TabIndex = 3;
             // 
             // txtCategoryName
@@ -135,6 +136,7 @@
             this.btnAdd.TabIndex = 9;
             this.btnAdd.Text = "Ekle";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnDelete
             // 
@@ -144,6 +146,7 @@
             this.btnDelete.TabIndex = 10;
             this.btnDelete.Text = "Sil";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -153,6 +156,7 @@
             this.btnUpdate.TabIndex = 11;
             this.btnUpdate.Text = "Güncelle";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnGetById
             // 
@@ -162,12 +166,13 @@
             this.btnGetById.TabIndex = 12;
             this.btnGetById.Text = "Id\'ye göre getir";
             this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
             // 
             // FrmCategory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(929, 369);
             this.Controls.Add(this.btnGetById);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnDelete);
--- CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CSharpEgitimKampi301.BusinessLayer.Abstract;
+using CSharpEgitimKampi301.BusinessLayer.Concrete;
+using CSharpEgitimKampi301.DataAccessLayer.EntityFramework;
+using CSharpEgitimKampi301.EntityLayer.Concreate;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +16,58 @@ namespace CSharpEgitimKampi301.PresentationLayer
 {
     public partial class FrmCategory : Form
     {
+        private readonly ICategoryService _categoryService;
+
         public FrmCategory()
         {
+            this._categoryService = new CategoryManager(new EfCategoryDal());
             InitializeComponent();
         }
+
+        
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var categoryvalues = _categoryService.TGetAll();
+            dataGridView1.DataSource= categoryvalues;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Category category = new Category();
+            category.CategoryName=txtCategoryName.Text;
+            category.CategoryStatus = true;
+            _categoryService.TInsert(category);
+            MessageBox.Show("Ekleme Başarılı");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCategortId.Text);
+            var deletedValues=_categoryService.TGetById(id);
+            _categoryService.TDelete(deletedValues);
+            MessageBox.Show("Silme Başarılı");
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            int id=int.Parse(txtCategortId.Text);
+            var values= _categoryService.TGetById(id);
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int updatedId=int.Parse(txtCategortId.Text);
+            var updatedValue = _categoryService.TGetById(updatedId);
+            updatedValue.CategoryName=txtCategoryName.Text;
+            updatedValue.CategoryStatus = true;
+            _categoryService.TUpdate(updatedValue);
         }
     }
 }
